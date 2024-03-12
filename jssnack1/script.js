6211b26b667e623774c2c2717faf21cfb8989073
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-12 19:03:27 +0100

    add-prompt

---
+++ jssnack1/script.js
@@ -0,0 +1,7 @@
+/* Create una sottocartella per ogni snack.
+- L'utente inserisce un numero nel prompt, se è pari stampa il numero,
+se è dispari stampa il numero successivo */
+
+// L'utente inserisce un numero nel prompt
+const numbers = parseInt(prompt('Hola'))
+console.log(numbers);
