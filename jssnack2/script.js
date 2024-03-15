cdaa3fccc6d84c1b686d938a4b821393e8162fd2
Brunei Paul Potosi Ccuno <neoxtrotoka@gmail.com>
2024-03-15 16:10:42 +0100

    complete-snack-1

---
+++ jssnack2/script.js
@@ -0,0 +1,19 @@
+/*- L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.*/
+
+//Chiedo due parole al'usuario
+//Se la prima parola e piu lunga stampo la seconda e poi la rpima
+
+//Se la seconda parola e piu lunga stampo la prima e poi la seconda
+//altrimenti stampo le parole uguali
+
+const firstWord = prompt(La prima parola e:)
+const secondWord = prompt(La seconda parola e:)
+console.log(firstWord);
+console.log(secondWord);
+
+if (secondWord > firstWord) {
+   userMessage = (secondWord + ' ' + firstWord);
+} else if () {
+    
+}
